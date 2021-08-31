4d57311748f57fb2ab2ab8a5a966a6b907289f55
pran saha <iampransaha@gmail.com>
2021-08-31 23:54:03 +0530

    Add project files.

---
+++ WPFDataDML/ContinentCountryState.xaml.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFDataDML
+{
+    /// <summary>
+    /// Interaction logic for ContinentCountryState.xaml
+    /// </summary>
+    public partial class ContinentCountryState : Window
+    {
+        SqlConnection connection;
+        ObservableCollection<Continent> continentList;
+        ObservableCollection<Country> countryList;
+        ObservableCollection<State> stateList;
+
+        public ContinentCountryState()
+        {
+            InitializeComponent();
+            continentList = new ObservableCollection<Continent>();
+            countryList = new ObservableCollection<Country>();
+            stateList = new ObservableCollection<State>();
+        }
+
+        class Continent
+        {
+            public int ContinentId { get; set; }
+            public string ContinentName { get; set; }
+        }
+        class Country
+        {
+            public string CountryId { get; set; }
+            public string CountryName { get; set; }
+            public string Capital { get; set; }
+            public int ContinentId { get; set; }
+            public BitmapImage Flag { get; set; }
+
+        }
+
+        class State
+        {
+            public string StateId { get; set; }
+            public string StateName { get; set; }
+            public string Capital { get; set; }
+        }
+
+        void GetContinents()
+        {
+            try
+            {
+                string sql = "select * from continent";
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    Continent continent = new Continent()
+                    {
+                        ContinentId = Convert.ToInt32(reader["ContinentId"]),
+                        ContinentName = reader["ContinentName"].ToString()
+                    };
+                    continentList.Add(continent);
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void MainGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            connection = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+            ContinentCombo.ItemsSource = continentList;
+            CountryCombo.ItemsSource = countryList;
+            StateCombo.ItemsSource = stateList;
+            GetContinents();
+        }
+
+        private void ContinentCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                countryList.Clear();
+                int continentId = Convert.ToInt32((sender as ComboBox).SelectedValue);
+                string sql = "select * from country where continentid = @conid";
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("conid", continentId);
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    Country country = new Country()
+                    {
+                        CountryId = reader["CountryId"].ToString(),
+                        Capital = reader["Capital"].ToString(),
+                        CountryName = reader["CountryName"].ToString()
+                    };
+                    Stream StreamObj = new MemoryStream((byte[])reader["Flag"]);
+                    BitmapImage BitObj = new BitmapImage();
+                    BitObj.BeginInit();
+                    BitObj.StreamSource = StreamObj;
+                    BitObj.EndInit();
+                    country.Flag = BitObj;
+                    countryList.Add(country);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void CountryCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if ((sender as ComboBox).SelectedValue != null)
+                {
+                    string countryId = (sender as ComboBox).SelectedValue.ToString();
+                    CountryPanel.DataContext = countryList.Where(country => country.CountryId == countryId).Single();
+
+                    stateList.Clear();
+
+                    string sql = "select * from state where countryId = @conid";
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    command.Parameters.AddWithValue("conid", countryId);
+                    if (connection.State == ConnectionState.Closed)
+                    {
+                        connection.Open();
+                    }
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+
+                        State state = new State()
+                        {
+                            StateName = reader["StateName"].ToString(),
+                            Capital = reader["StateCapital"].ToString(),
+                            StateId = reader["StateId"].ToString()
+                        };
+                        stateList.Add(state);
+                    }
+                }
+                else
+                {
+                    CountryPanel.DataContext = null;
+                    StatePanel.DataContext = null;
+                    stateList.Clear();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void StateCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((sender as ComboBox).SelectedValue != null)
+            {
+                string stateId = (sender as ComboBox).SelectedValue.ToString();
+                StatePanel.DataContext = stateList.Where(state => state.StateId == stateId).Single();
+            }
+            else
+            {
+                StatePanel.DataContext = null;
+            }
+        }
+    }
+}
---
+++ WPFDataDML/MainWindow.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DBlibrary;
+
+namespace WPFDataDML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        EmpDataStore empData;
+        public MainWindow()
+        {
+            InitializeComponent();
+            empData = new EmpDataStore(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        }
+
+        private void ReloadEmpDataGrid()
+        {
+            List<Employee> empList = empData.GetAllEmps();
+            DataGridEmp.ItemsSource = null;
+            DataGridEmp.ItemsSource = empList;
+        }
+
+        private void MainGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            ReloadEmpDataGrid();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (EmpNoTxt.Text == "")
+                {
+                    MessageBox.Show("Enter Empno");
+                    return;
+                }
+                Employee emp = new Employee()
+                {
+                    EmpName = EmpNameTxt.Text,
+                    Empno = Convert.ToInt32(EmpNoTxt.Text),
+                    Hiredate = Convert.ToDateTime(HireDateTxt.Text),
+                    Salary = Convert.ToDecimal(SalaryTxt.Text)
+                };
+
+                if (empData.InsertEmpDetails(emp))
+                {
+                    MessageBox.Show("Successfully inserted");
+                    ReloadEmpDataGrid();
+                }
+                else
+                {
+                    MessageBox.Show("Not inserted");
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (FormatException fex)
+            {
+                MessageBox.Show(fex.Message);
+            }
+        }
+
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (EmpNoTxt.Text == "")
+                {
+                    MessageBox.Show("Enter Empno");
+                    return;
+                }
+                Employee emp = new Employee()
+                {
+                    EmpName = EmpNameTxt.Text,
+                    Empno = Convert.ToInt32(EmpNoTxt.Text),
+                    Hiredate = Convert.ToDateTime(HireDateTxt.Text),
+                    Salary = Convert.ToDecimal(SalaryTxt.Text)
+                };
+                if (empData.UpdateEmpByNo(emp))
+                {
+                    MessageBox.Show("Successfully Updated");
+                    ReloadEmpDataGrid();
+                }
+                else
+                {
+                    MessageBox.Show("Not Updated");
+                }
+            }
+            catch (FormatException fex)
+            {
+                MessageBox.Show(fex.Message);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Do you wand to delete Entry ?", "Delete Confirmation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    if (EmpNoTxt.Text == "")
+                    {
+                        MessageBox.Show("Enter Empno To delete");
+                        return;
+                    }
+                    int empno = Convert.ToInt32(EmpNoTxt.Text);
+                    if (empData.DeleteEmpByNo(empno))
+                    {
+                        MessageBox.Show("Successfully Deleted");
+                        ReloadEmpDataGrid();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not Deleted");
+                    }
+                }
+                catch(FormatException fex)
+                {
+                    MessageBox.Show(fex.Message);
+                }
+                catch(SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            EmpNameTxt.Text = "";
+            EmpNoTxt.Text = "";
+            HireDateTxt.Text = "";
+            SalaryTxt.Text = "";
+            DataGridEmp.ItemsSource = null;
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (EmpNoTxt.Text == "")
+                {
+                    MessageBox.Show("Please Enter Empno To Search");
+                    return;
+                }
+                List<Employee> empList = new List<Employee>() { empData.GetEmployeeByNo(Convert.ToInt32(EmpNoTxt.Text)) };
+                DataGridEmp.ItemsSource = null;
+                DataGridEmp.ItemsSource = empList;
+            }
+            catch(FormatException fex)
+            {
+                MessageBox.Show(fex.Message);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
